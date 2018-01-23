5eea85b2cb37611bbddbf763af7cb728db952fc3
怪兽产品 <zhangdeyang@enmonster.com>
2018-01-23 13:31:46 +0800

    修改

--- components/Emoji/emoji.js
+++ components/Emoji/emoji.js
@@ -41,33 +41,38 @@ Component({
         },
         //选择表情
         _choose: function (e) {
-            // console.info(e.currentTarget.dataset.id)
             //将选择器的选取范围更改为自定义组件component内。（初始时，选择器仅选取页面范围的节点，不会选取任何自定义组件中的节点。）
             // const query = wx.createSelectorQuery().in(this);
-            this.triggerEvent("customevent", { value: e.currentTarget.dataset.id })
+            this.triggerEvent("customevent", { value: e.currentTarget.dataset.code })
         },
+
+
         //遍历emoji文件
         getEmoji: function () {
             let pathArr = [[], [], [], [], [], []];
             for (let i = 1; i < 135; i++) {
-                let path = '';
+                let pa = {
+                    path : '',
+                    code : ''
+                };
                 if (i < 10) {
-                    path = `/libs/wxParse/emojis/0${i}.gif`;
+                    pa.path = `/libs/wxParse/emojis/0${i}.gif`;
                 } else {
-                    path = `/libs/wxParse/emojis/${i}.gif`;
+                    pa.path = `/libs/wxParse/emojis/${i}.gif`;
                 }
+                pa.code = `[em_${i}]`;
                 if (0 <= i && i < 25) {
-                    pathArr[0].push(path)
+                    pathArr[0].push(pa)
                 } else if (24 <= i && i < 49) {
-                    pathArr[1].push(path)
+                    pathArr[1].push(pa)
                 } else if (48 <= i && i < 73) {
-                    pathArr[2].push(path)
+                    pathArr[2].push(pa)
                 } else if (72 <= i && i < 97) {
-                    pathArr[3].push(path)
+                    pathArr[3].push(pa)
                 } else if (96 <= i && i < 121) {
-                    pathArr[4].push(path)
+                    pathArr[4].push(pa)
                 } else {
-                    pathArr[5].push(path)
+                    pathArr[5].push(pa)
                 }
                 // pathArr.push(path)
             }
